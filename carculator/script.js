6c173ae8298b081e56bce6119f3eb93875a93e02
L1m3Kun <sungku2757@gmail.com>
2024-10-24 18:54:07 +0900

    lv3: 기본 구현사항 2번

--- carculator/script.js
+++ carculator/script.js
@@ -1,9 +1,16 @@
 // 버튼 요소 배열 생성
 const buttons = document.querySelectorAll(".button");
+// display 내부 요소 변수에 저장 및 내부 초기화
+const display = document.querySelector(".calc-content");
+display.innerText = "0";
 
 // 버튼 클릭 이벤트 콜백 함수 선언
 const handleButtonClick = (event) => {
-  console.log(event.target.innerText);
+  // 내부에 'number'가 있다면 숫자를 display에 도시
+  if (Object.values(event.target.classList).includes("number")) {
+    if (display.innerText === "0") display.innerText = "";
+    display.innerText += event.target.innerText;
+  }
 };
 
 // 모든 버튼 요소에 이벤트 등록
