68f498b19147f59cd652ece8c6674c0e8e873875
L1m3Kun <sungku2757@gmail.com>
2024-10-24 18:41:26 +0900

    lv3: 주석추가

--- carculator/script.js
+++ carculator/script.js
@@ -1,11 +1,18 @@
+// 버튼 요소 배열 생성
 const buttons = document.querySelectorAll(".button");
 
+// 버튼 클릭 이벤트 콜백 함수 선언
 const handleButtonClick = (event) => {
   console.log(event.target.innerText);
 };
 
-// buttons.addEventListener("click", handleButtonClick);
-
+// 모든 버튼 요소에 이벤트 등록
 buttons.forEach((button) => {
   button.addEventListener("click", handleButtonClick);
 });
+
+/** 이벤트 위임을 활용한 이벤트 등록
+ * const buttons = document.querySelector(".buttons");
+ *
+ * buttons.addEventListener("click", handleButtonClick);
+ */
